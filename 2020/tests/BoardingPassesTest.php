2aef47044eb57082db96195145b5ebbd32127418
sprak3000 <sprak3000+github@gmail.com>
2020-12-15 10:41:20 -0500

    2020 Day 5 Part 1 solved

---
+++ 2020/tests/BoardingPassesTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace adventofcode\Year2020\Test;
+
+use adventofcode\Year2020\BoardingPasses;
+use PHPUnit\Framework\TestCase;
+
+class BoardingPassesTest extends TestCase
+{
+    protected $boardingPasses;
+
+    protected function setup(): void
+    {
+        $this->boardingPasses = new BoardingPasses();
+    }
+
+    public function testComputeSeatData()
+    {
+        // Day 5 Part 1 example inputs
+        $passes = [
+            [
+                'pass' => 'FBFBBFFRLR',
+                'expectedResult' => [
+                    'row' => 44,
+                    'column' => 5,
+                    'id' => 357,
+                ],
+            ],
+            [
+                'pass' => 'BFFFBBFRRR',
+                'expectedResult' => [
+                    'row' => 70,
+                    'column' => 7,
+                    'id' => 567,
+                ],
+            ],
+            [
+                'pass' => 'FFFBBBFRRR',
+                'expectedResult' => [
+                    'row' => 14,
+                    'column' => 7,
+                    'id' => 119,
+                ],
+            ],
+            [
+                'pass' => 'BBFFBBFRLL',
+                'expectedResult' => [
+                    'row' => 102,
+                    'column' => 4,
+                    'id' => 820,
+                ],
+            ],
+        ];
+
+        foreach ($passes as $pass) {
+            $data = $this->boardingPasses->computeSeatData($pass['pass']);
+            $this->assertSame($pass['expectedResult'], $data);
+        }
+    }
+
+    public function testFindHighestSeatID()
+    {
+        // Day 5 Part 1 example inputs
+        $passes = [
+            'FBFBBFFRLR',
+            'BFFFBBFRRR',
+            'FFFBBBFRRR',
+            'BBFFBBFRLL',
+        ];
+
+        $id = $this->boardingPasses->findHighestSeatID($passes);
+
+        $this->assertSame(820, $id);
+
+        // Day 2 Part 1 puzzle inputs
+//        $entries = file(__DIR__ . '/../inputs/day-02.input', FILE_IGNORE_NEW_LINES);
+//        $result = $this->passwords->validSledRentalPasswords($entries);
+//
+//        $this->assertCount(477, $result);
+    }
+}
